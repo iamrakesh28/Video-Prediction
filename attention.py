4e10eff944d4612233ade6a7f0184809ad29c87b
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-03-23 23:21:05 +0530

    Add call to parent class constructor

--- attention.py
+++ attention.py
@@ -4,15 +4,16 @@ class Attention(tf.keras.layers.Layer):
     
     def __init__(self, input_shape):
         # Assumption : query.shape = values.shape = key.shape
-        self.input_shape = input_shape
-        self.softmax = tf.keras.layers.Softmax()
+        super(Attention, self).__init__()
+        self.__input_shape = input_shape
+        self.__softmax = tf.keras.layers.Softmax()
     
     def call(self, query, keys, values):
         # query shape = (batch_size, rows, cols, height)
         
-        assert(query.shape == keys.shape == values.shape == self.input_shape)
+        assert(query.shape == keys.shape == values.shape == self.__input_shape)
         
-        no_keys = no_query = self.input_shape[3]
+        no_keys = no_query = self.__input_shape[3]
         weights = []
         
         for q in range(no_query):
@@ -25,13 +26,13 @@ class Attention(tf.keras.layers.Layer):
                 activation.append(dot_prod)
                 
             activation = tf.transpose(tf.convert_to_tensor(activation))
-            activation = self.softmax(activation)
+            activation = self.__softmax(activation)
             
-            weight = tf.zeros([self.input_shape[0], self.input_shape[1], self.input_shape[2]])
+            weight = tf.zeros([self.__input_shape[0], self.__input_shape[1], self.__input_shape[2]])
             for k in range(no_keys):
                 score = tf.broadcast_to(
-                    tf.reshape(activation[:, k], [self.input_shape[0], 1, 1]),
-                    [self.input_shape[0], self.input_shape[1], self.input_shape[2]])
+                    tf.reshape(activation[:, k], [self.__input_shape[0], 1, 1]),
+                    [self.__input_shape[0], self.__input_shape[1], self.__input_shape[2]])
                 weight += score * values[:, :, :, k]
             
             weights.append(weight)
