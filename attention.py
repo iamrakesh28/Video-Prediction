5b294b68a68aa58c849ebd64fb6eb84495d38876
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-04-06 16:02:51 +0530

    Correct attention equation

--- attention.py
+++ attention.py
@@ -6,39 +6,41 @@ class Attention(tf.keras.layers.Layer):
         # Assumption : query.shape = values.shape = key.shape
         super(Attention, self).__init__()
         self.__input_shape = None
-        self.__softmax = tf.keras.layers.Softmax()
+        self.softmax = tf.keras.layers.Softmax()
     
     def call(self, query, keys, values):
-        # query shape = (batch_size, rows, cols, height)
+        # query shape = (batch_size, time-steps, rows, cols, channels)
         
         assert(query.shape == keys.shape == values.shape)
         self.__input_shape = query.shape
         
-        no_keys = no_query = self.__input_shape[3]
+        dim_querys = dim_keys = self.__input_shape[1]
         weights = []
         
-        for q in range(no_query):
+        for q in range(dim_querys):
             
             activation = []
-            for k in range(no_keys):
-                dot_prod = query[:, :, :, q] * keys[:, :, :, k]
-                dot_prod = tf.reduce_sum(tf.reduce_sum(dot_prod, 1), 1)
+            for k in range(dim_keys):
+                dot_prod = query[:, q, :, :, :] * keys[:, k, :, :, :]
+                dot_prod = tf.reduce_sum(tf.reduce_sum(tf.reduce_sum(dot_prod, 1), 1), 1)
                 
                 activation.append(dot_prod)
                 
             activation = tf.transpose(tf.convert_to_tensor(activation))
-            activation = self.__softmax(activation)
+            activation = self.softmax(activation / np.sqrt(dim_keys))
             
-            weight = tf.zeros([self.__input_shape[0], self.__input_shape[1], self.__input_shape[2]])
-            for k in range(no_keys):
+            weight = tf.zeros(
+                [self.__input_shape[0], self.__input_shape[2], self.__input_shape[3], self.__input_shape[4]]
+            )
+            for k in range(dim_keys):
                 score = tf.broadcast_to(
-                    tf.reshape(activation[:, k], [self.__input_shape[0], 1, 1]),
-                    [self.__input_shape[0], self.__input_shape[1], self.__input_shape[2]])
+                    tf.reshape(activation[:, k], [self.__input_shape[0], 1, 1, 1]),
+                    [self.__input_shape[0], self.__input_shape[2], self.__input_shape[3], self.__input_shape[4]])
                 
-                weight += score * values[:, :, :, k]
+                weight += score * values[:, k, :, :, :]
             
             weights.append(weight)
         
-        weights = tf.transpose(tf.convert_to_tensor(weights), [1, 2, 3, 0])
+        weights = tf.transpose(tf.convert_to_tensor(weights), [1, 0, 2, 3, 4])
             
         return weights
