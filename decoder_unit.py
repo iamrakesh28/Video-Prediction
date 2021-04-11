27e8c1af65e48e8faf517bba5f77dcf4a8740f9f
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-04-11 12:07:29 +0530

    Add comments and working code

--- decoder_unit.py
+++ decoder_unit.py
@@ -4,82 +4,76 @@ from attention import Attention
 
 class DecoderUnit(tf.keras.layers.Layer):
     
-    def __init__(self, num_heads, encoder_num_imgs, decoder_num_imgs, filter_size):
+    def __init__(self, num_heads, d_model, filters, filter_size, time_steps):
         super(DecoderUnit, self).__init__()
-        self.__num_decoder_atten = decoder_num_imgs
-        self.__num_heads = num_heads
-        self.__atten_conv2d = []
-        self.__mask_atten_conv2d = []
-        self.__encoder_num_imgs = encoder_num_imgs
-        self.__decoder_num_imgs = decoder_num_imgs
-        self.__filter_size = filter_size
-        self.__attention = Attention()
-        self.__mask_attention = Attention()
-        self.__norm1 = tf.keras.layers.BatchNormalization()
-        self.__norm2 = tf.keras.layers.BatchNormalization()
-        self.__norm3 = tf.keras.layers.BatchNormalization()
+        self.num_heads = num_heads
+        self.d_model = d_model
+        self.atten_weights = []
+        self.mask_atten_weights = []
+        self.filters = filters + [d_model]
+        self.filter_size = filter_size
+        self.time_steps = time_steps
+        self.attention = Attention()
+        self.mask_attention = Attention()
+        self.norm1 = tf.keras.layers.BatchNormalization()
+        self.norm2 = tf.keras.layers.BatchNormalization()
+        self.norm3 = tf.keras.layers.BatchNormalization()
         
-        # filter number may be changed
-        self.__layer1 = tf.keras.layers.Conv2D(
-            decoder_num_imgs + encoder_num_imgs, 
-            filter_size,
-            activation='relu',
-            padding='same'
-        )
-        self.__layer2 = tf.keras.layers.Conv2D(
-            decoder_num_imgs, 
-            filter_size, 
-            padding='same',
-            activation='relu'
-        )
+        self.layers = []
+        for layer in range(len(self.filters)):
+            conv_layer = tf.keras.layers.Conv2D(
+                self.filters[layer], 
+                filter_size, 
+                activation='relu',
+                padding='same'
+            )
+            self.layers.append(conv_layer)
         
         # for now, num_heads = 1
         assert(num_heads > 0)
         
         for head in range(num_heads):
             
-            self.__atten_conv2d.append([])
+            self.atten_weights.append([])
             for qkv in range(3):
-                conv2d = tf.keras.layers.Conv2D(
-                    decoder_num_imgs + encoder_num_imgs, 
-                    filter_size, 
-                    padding='same'
-                )
-                self.__atten_conv2d[head].append(conv2d)
+                weight = tf.keras.layers.Conv2D(d_model, filter_size, padding='same')
+                self.atten_weights[head].append(weight)
                 
         for head in range(num_heads):
             
-            self.__mask_atten_conv2d.append([])
+            self.mask_atten_weights.append([])
             for qkv in range(3):
-                conv2d = tf.keras.layers.Conv2D(decoder_num_imgs, filter_size, padding='same')
-                self.__mask_atten_conv2d[head].append(conv2d)
+                weight = tf.keras.layers.Conv2D(d_model, filter_size, padding='same')
+                self.mask_atten_weights[head].append(weight)
                 
         
     def call(self, encoder_inputs, decoder_inputs, training=True):
         
-        # input shape = (batch_size, rows, cols, height)
-        assert(decoder_inputs.shape[3] <= self.__num_decoder_atten)
-        if (decoder_inputs.shape[3] < self.__num_decoder_atten):
+        # input shape = (batch_size, time-steps, rows, cols, channels)
+        if (decoder_inputs.shape[1] < self.time_steps):
             shape = decoder_inputs.shape
-            zeros = tf.zeros([shape[0], shape[1], shape[2], self.__num_decoder_atten - shape[3]])
-            decoder_inputs = tf.concat([decoder_inputs, zeros], axis=3)
+            zeros = tf.zeros([shape[0], self.time_steps - shape[1], shape[2], shape[3], shape[4]])
+            decoder_inputs = tf.concat([decoder_inputs, zeros], axis=1)
             
         # masked decoder attention
-        query = self.__mask_atten_conv2d[0][0](decoder_inputs)
-        keys = self.__mask_atten_conv2d[0][1](decoder_inputs)
-        values = self.__mask_atten_conv2d[0][2](decoder_inputs)
+        query = self.mask_atten_weights[0][0](decoder_inputs)
+        keys = self.mask_atten_weights[0][1](decoder_inputs)
+        values = self.mask_atten_weights[0][2](decoder_inputs)
         
-        mask_values = self.__norm1(self.__mask_attention(query, keys, values), training=training)
-        inputs = tf.concat([encoder_inputs, mask_values], axis=3)
+        mask_values = self.norm1(self.mask_attention(query, keys, values), training=training)
+        inputs = tf.concat([encoder_inputs, mask_values], axis=1)
 
         
         # encoder decoder attention
-        query = self.__atten_conv2d[0][0](inputs)
-        keys = self.__atten_conv2d[0][1](inputs)
-        values = self.__atten_conv2d[0][2](inputs)
+        query = self.atten_weights[0][0](inputs)
+        keys = self.atten_weights[0][1](inputs)
+        values = self.atten_weights[0][2](inputs)
         
         
-        attentuated = self.__norm2(self.__attention(query, keys, values), training=training)
+        attentuated = self.norm2(self.attention(query, keys, values), training=training)
         
         # feed forward
-        return self.__norm3(self.__layer2(self.__layer1(attentuated)), training=training)
+        for layer in range(len(self.filters)):
+            attentuated = self.layers[layer](attentuated)
+            
+        return self.norm3(attentuated, training=training)
