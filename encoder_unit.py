463d51a92f46a4e84ee65f8053080180241ac2c7
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-04-11 12:06:19 +0530

    Add comments, fix bugs and more generalization

--- encoder_unit.py
+++ encoder_unit.py
@@ -4,50 +4,55 @@ from attention import Attention
 
 class EncoderUnit(tf.keras.layers.Layer):
     
-    def __init__(self, num_heads, num_images, filter_size):
+    '''
+        @num_heads number of attention heads
+        @d_model channel dimension (equivalent to d_model in 'Attention is all you need')
+        @filter filter numbers for feed forward layers [f1, f2, .., fn]
+    '''
+    def __init__(self, num_heads, d_model, filters, filter_size):
         super(EncoderUnit, self).__init__()
-        self.__num_heads = num_heads
-        self.__atten_conv2d = []
-        self.__num_images = num_images
-        self.__filter_size = filter_size
-        self.__attention = Attention()
-        self.__norm1 = tf.keras.layers.BatchNormalization()
-        self.__norm2 = tf.keras.layers.BatchNormalization()
-        # filter number may be changed
-        self.__layer1 = tf.keras.layers.Conv2D(
-            2 * num_images, 
-            filter_size, 
-            activation='relu',
-            padding='same'
-        )
+        self.num_heads = num_heads
+        self.atten_weights = []
+        self.d_model = d_model
+        self.filter_size = filter_size
+        self.filters = filters + [d_model]
+        self.attention = Attention()
+        self.norm1 = tf.keras.layers.BatchNormalization()
+        self.norm2 = tf.keras.layers.BatchNormalization()
         
-        self.__layer2 = tf.keras.layers.Conv2D(
-            num_images, 
-            filter_size, 
-            activation='relu',
-            padding='same'
-        )
+        self.layers = []
+        for layer in range(len(self.filters)):
+            conv_layer = tf.keras.layers.Conv2D(
+                self.filters[layer], 
+                filter_size, 
+                activation='relu',
+                padding='same'
+            )
+            self.layers.append(conv_layer)
         
         # for now, num_heads = 1
         assert(num_heads > 0)
         
         for head in range(num_heads):
             
-            self.__atten_conv2d.append([])
+            self.atten_weights.append([])
             for qkv in range(3):
-                conv2d = tf.keras.layers.Conv2D(num_images, filter_size, padding='same')
-                self.__atten_conv2d[head].append(conv2d)
+                weight = tf.keras.layers.Conv2D(d_model, filter_size, padding='same')
+                self.atten_weights[head].append(weight)
                 
         
     def call(self, inputs, training=True):
         
         # self-attention
-        query = self.__atten_conv2d[0][0](inputs)
-        keys = self.__atten_conv2d[0][1](inputs)
-        values = self.__atten_conv2d[0][2](inputs)
+        query = self.atten_weights[0][0](inputs)
+        keys = self.atten_weights[0][1](inputs)
+        values = self.atten_weights[0][2](inputs)
         
         
-        attentuated = self.__norm1(self.__attention(query, keys, values), training=training)
+        attentuated = self.norm1(self.attention(query, keys, values), training=training)
         
         # feed forward
-        return self.__norm2(self.__layer2(self.__layer1(attentuated)), training=training)
+        for layer in range(len(self.filters)):
+            attentuated = self.layers[layer](attentuated)
+            
+        return self.norm2(attentuated, training=training)
