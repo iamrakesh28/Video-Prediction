265991c988e5d26fd22442d42bc4fa343044d01a
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-04-11 12:10:21 +0530

    Change final CNN layer to 3D

--- decoder.py
+++ decoder.py
@@ -4,29 +4,44 @@ from decoder_unit import DecoderUnit
 
 class Decoder(tf.keras.Model):
     
-    def __init__(self, num_heads, dec_layers, encoder_num_imgs, decoder_num_imgs, filter_sz):
+    def __init__(
+        self, 
+        num_heads, 
+        dec_layers, 
+        d_model,  
+        filters, 
+        filter_size, 
+        enc_steps,
+        dec_steps,
+        out_channels,
+    ):
         super(Decoder, self).__init__()
         
-        self.__num_heads  = num_heads
-        self.__dec_layers = dec_layers
-        self.__filter_sz  = filter_sz
-        # self.__input_shape= input_shape
-        self.__decoder_units  = []
-        self.__final_conv = tf.keras.layers.Conv2D(
-            1, 
-            filter_sz, 
-            padding='same',
-            activation='relu'
+        self.num_heads  = num_heads
+        self.dec_layers = dec_layers
+        self.filters  = filters
+        self.filter_size = filter_size
+        self.enc_steps = enc_steps
+        self.dec_steps = dec_steps
+        self.out_channels = out_channels
+        self.total_steps = dec_steps + dec_layers * enc_steps
+        self.decoder_units  = []
+        
+        self.final_conv = tf.keras.layers.Conv3D(
+            out_channels, 
+            (self.total_steps, 1, 1), 
+            padding='valid',
+            activation='sigmoid',
         )
     
         for layer in range(dec_layers):
-            decoder_unit = DecoderUnit(num_heads, encoder_num_imgs, decoder_num_imgs, filter_sz)
+            decoder_unit = DecoderUnit(num_heads, d_model, filters, filter_size, dec_steps)
             
-            self.__decoder_units.append(decoder_unit)
+            self.decoder_units.append(decoder_unit)
             
     def call(self, enc_values, dec_values, training=True):
         
-        for layer in range(self.__dec_layers):
-            dec_values = self.__decoder_units[layer](enc_values, dec_values, training)
+        for layer in range(self.dec_layers):
+            dec_values = self.decoder_units[layer](enc_values, dec_values, training)
         
-        return self.__final_conv(dec_values)
+        return self.final_conv(dec_values)
