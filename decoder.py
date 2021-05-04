bd9e5d779c83584ce3ef5eae5fc1d79ef36f0d12
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-05-04 16:00:21 +0530

    Complete decoder call method

--- decoder.py
+++ decoder.py
@@ -1,47 +1,43 @@
 import tensorflow as tf
 
-from decoder_unit import DecoderUnit
+from utility import positional_encoding
+from decoder_layer import DecoderLayer
 
-class Decoder(tf.keras.Model):
+class Decoder(tf.keras.layers.Layer):
     
-    def __init__(
-        self, 
-        num_heads, 
-        dec_layers, 
-        d_model,  
-        filters, 
-        filter_size, 
-        enc_steps,
-        dec_steps,
-        out_channels,
-    ):
+    def __init__(self, num_layers, d_model, num_heads, dff, filter_size,
+                 image_shape, max_position_encoding):
         super(Decoder, self).__init__()
         
-        self.num_heads  = num_heads
-        self.dec_layers = dec_layers
-        self.filters  = filters
-        self.filter_size = filter_size
-        self.enc_steps = enc_steps
-        self.dec_steps = dec_steps
-        self.out_channels = out_channels
-        self.total_steps = dec_steps + dec_layers * enc_steps
-        self.decoder_units  = []
+        self.num_layers = num_layers
+        self.d_model = d_model
+
+        self.embedding = tf.keras.layers.Conv2D(d_model, filter_size, padding='same')
+        self.pos_encoding = positional_encoding(max_position_encoding, image_shape, d_model)
         
-        self.final_conv = tf.keras.layers.Conv3D(
-            out_channels, 
-            (self.total_steps, 1, 1), 
-            padding='valid',
-            activation='sigmoid',
-        )
+        self.dec_layers  = [DecoderLayer(d_model, num_heads, dff, filter_size)
+                            for _ in range(num_layers)]
     
-        for layer in range(dec_layers):
-            decoder_unit = DecoderUnit(num_heads, d_model, filters, filter_size, dec_steps)
-            
-            self.decoder_units.append(decoder_unit)
-            
-    def call(self, enc_values, dec_values, training=True):
+
+    def call(self, x, enc_output, training, look_ahead_mask):
+
+        # enc_output.shape = (batch_size, input_seq_len, rows, cols, depth)
         
-        for layer in range(self.dec_layers):
-            dec_values = self.decoder_units[layer](enc_values, dec_values, training)
+        seq_len = x.shape[1]
+        attention_weights = {}
         
-        return self.final_conv(dec_values)
+        # image embedding and position encoding
+        x = self.embedding(x) # (batch_size, target_seq_len, rows, cols, depth)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :, :, :]
+        
+        for layer in range(self.num_layers):
+            x, block1, block2 = self.dec_layers[layer](x, enc_output,
+                                                       training, look_ahead_mask)
+
+            attention_weights[f'decoder_layer{layer+1}_block1'] = block1
+            attention_weights[f'decoder_layer{layer+1}_block2'] = block2
+
+        # x.shape = (batch_size, target_seq_len, rows, cols, d_model)
+        return x, attention_weights
+
